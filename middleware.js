5ae43bac9a58711f2471b9366db47db5e843e4e9
Andrés <51636451+FairBreakPG@users.noreply.github.com>
2024-11-18 21:42:09 -0600

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,26 +1,19 @@
 import jwt from 'jsonwebtoken';
-import { JWT_SECRETA } from './consultas';
 
 export const authenticateToken = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
+  const token = req.headers['authorization'];
 
-    if (token == null) {
-        return res.status(401).json({ message: 'Token no proporcionado' });
+  
+  if (!token) {
+    return res.status(401).json({ message: 'Acceso denegado' });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: 'Token no válido' });
     }
 
-    jwt.verify(token, JWT_SECRETA, (err, user) => {
-        if (err) {
-            console.log('Error en la verificación del token:', err.message);
-            return res.status(403).json({ message: 'Token inválido' });
-        }
-
-        // Asignar el usuario y su rol a req.user
-        req.user = {
-            id: user.id,  // Asegúrate de que el token tenga el campo 'id'
-            role: user.role // Asegúrate de que el token tenga el campo 'role'
-        };
-
-        next();
-    });
+    req.user = user;
+    next();
+  });
 };
